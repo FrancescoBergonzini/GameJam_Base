ad75d67a4d805a4d8690bed252a689103a49a9ed
Francesco Ber <fbergonzinigames@gmail.com>
2023-07-15 14:54:04 +0200

    add SerializebleDictionary add Sandbox folder

---
+++ GameJam_2023/Assets/Sandbox/test_code.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_code : MonoBehaviour
+{
+    public SerializableDictionary<float, int> marco;
+}
