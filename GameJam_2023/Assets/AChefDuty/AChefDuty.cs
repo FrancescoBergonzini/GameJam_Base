6348c424001962ee67f47803ef84d3185bab002d
Francesco Ber <fbergonzinigames@gmail.com>
2023-07-21 16:21:36 +0200

    food spawn

--- GameJam_2023/Assets/AChefDuty/AChefDuty.cs
+++ GameJam_2023/Assets/AChefDuty/AChefDuty.cs
@@ -1,25 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace GameJamCore
 {
     public class AChefDuty : GameManagerBase
     {
-        public Transform[] food_spawn_position;
+        public List<Transform> food_spawn_position;
 
         [SerializeField] FoodConfig[] _food_config;
 
         public Dictionary<foodtype, FoodConfig> food_configs;
 
-        private void Start()
-        {
-            //inizialize dictionary
+        public List<int> availableNumbers = new List<int>();
 
+
+    private void Start()
+        {
+            food_configs = new Dictionary<foodtype, FoodConfig>();
+            //inizialize dictionary
             foreach(FoodConfig food in _food_config)
             {
                 food_configs.Add(food.type, food);
             }
+
+            //numbers
+            for (int i = 0; i <= 9; i++)
+            {
+                availableNumbers.Add(i);
+            }
+
+            SpawnFood();
+        }
+
+
+        public void SpawnFood()
+        {
+            var clone_foods = food_configs.Values.ToList();
+
+            for(int i = 0; i < food_configs.Values.Count; i++)
+            {
+                int randomIndex = Random.Range(0, availableNumbers.Count);
+                int randomNumber = availableNumbers[randomIndex];
+
+  
+                FoodObject.Create(clone_foods[randomNumber], food_spawn_position[randomNumber].position);
+
+                //remove
+                availableNumbers.Remove(randomNumber);
+
+            }
+
+            Debug.Log("Complete");
+
+        }
+
+        public void SpawnFood(foodtype[] foods)
+        {
+
         }
     }
 }
--- GameJam_2023/Assets/AChefDuty/Chef/CharacterChef.cs
+++ GameJam_2023/Assets/AChefDuty/Chef/CharacterChef.cs
@@ -9,6 +9,12 @@ namespace GameJamCore
     {
         public static CharacterChef instance;
 
+        public FoodObject current_food_holding;
+
+        public List<GameObject> _food_holding_pref;
+
+        public Dictionary<foodtype, GameObject> food_holding_pref;
+
         public void Awake()
         {
             instance = this;
