6a8959cbfaabe549b0973d504ed4ae1f42a0943e
Francesco Ber <fbergonzinigames@gmail.com>
2023-08-01 09:27:54 +0200

    add quit and change icon

--- GameJam_2023/Assets/AChefDuty/AChefDuty.cs
+++ GameJam_2023/Assets/AChefDuty/AChefDuty.cs
@@ -89,6 +89,11 @@ namespace GameJamCore
 
         private void Update()
         {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
             if (Input.GetKeyDown(KeyCode.R))
             {
                 RestartScene();
