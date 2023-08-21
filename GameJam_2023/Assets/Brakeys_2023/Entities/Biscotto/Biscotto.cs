0e7cfacc67d8299293d91391d4a559f4722f87d6
Francesco Ber <fbergonzinigames@gmail.com>
2023-08-21 15:52:46 +0200

    add basic biscotto spawn and integrity logic

--- GameJam_2023/Assets/Brakeys_2023/Entities/Biscotto/Biscotto.cs
+++ GameJam_2023/Assets/Brakeys_2023/Entities/Biscotto/Biscotto.cs
@@ -1,19 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace GameJamCore.Brakeys_2023
 {
+
     public class Biscotto : GameEntity
     {
         Rigidbody2D _rdb;
         Collider _col;
+
+        [Space]
+        float _integrity;
+        public float Integrity
+        {
+            get { return _integrity; }
+
+            //l'integrity va da 0 a 100
+            protected set 
+            {
+                _integrity = value;
+
+                if (_integrity < 0) _integrity = 0;
+                if (_integrity > 100) _integrity = 100;
+
+            }
+        }
+
+
+        public static int ActiveInGame = 0;
 
         public static Biscotto Create(Biscotto prefab, Vector3 position, Quaternion rotation, Vector2 force, Transform parent)
         {
             Biscotto _biscotto = Instantiate(prefab, position, rotation, parent);
 
             _biscotto.Inizialize();
+
+            ActiveInGame++;
 
             return _biscotto;
         }
@@ -26,7 +50,34 @@ namespace GameJamCore.Brakeys_2023
 
             if (_col == null)
                 _col = GetComponent<Collider>();
+
+            //Sempre 100 di base?
+            SetIntegrity(100f);
+
         }
+
+        public Rigidbody2D GetRigidbody()
+        {
+            if (_rdb != null) return _rdb;
+            else return null;
+        }
+
+        #region Integrity
+
+        public void SetIntegrity(float value)
+        {
+            Integrity = value;
+        }
+
+        public void ModifyIntegrity(float value)
+        {
+            Integrity += value;
+        }
+
+        #endregion
+
+
+
     }
 }
 
--- GameJam_2023/Assets/Brakeys_2023/GameManager.cs
+++ GameJam_2023/Assets/Brakeys_2023/GameManager.cs
@@ -7,6 +7,39 @@ namespace GameJamCore.Brakeys_2023
     public class GameManager : GameManagerBase
     {
 
+        [Header("Game references")]
+        public Transform biscotto_parent;
+
+
+        [Header("Biscotto references")]
+        public Biscotto biscotto_prefab;
+
+        private void Start()
+        {
+            //test
+            BiscottoSpawn();
+        }
+
+
+        #region Biscotto Manage
+
+        public Vector2 GetBiscottoSpawnPosition()
+        {
+            return Vector2.zero;
+        }
+
+
+        public Biscotto BiscottoSpawn()
+        {
+            return Biscotto.Create(prefab: biscotto_prefab,
+                                  position: Vector3.zero,
+                                  rotation: Quaternion.identity,
+                                  force: GetBiscottoSpawnPosition(),
+                                  parent: biscotto_parent);
+        }
+
+        #endregion
+
     }
 }
 
