5b3c63feaaa5464190553eab006ef14fb4d8f9cf
Francesco Ber <fbergonzinigames@gmail.com>
2023-08-26 22:37:26 +0200

    add sounds enum

--- GameJam_2023/Assets/Brakeys_2023/Brakeys_AudioManager.cs
+++ GameJam_2023/Assets/Brakeys_2023/Brakeys_AudioManager.cs
@@ -9,7 +9,23 @@ namespace GameJamCore.Brakeys_2023
         None = 0,
         notImplemented,
 
-        test_sound = 10
+        test_sound = 10,
+
+        //
+        splash_biscuits = 20,
+        active_claw = 21,
+        closing_claw = 22,
+        biscuit_to_points = 23,
+
+        //ui
+        buttons_menu_0 = 30,
+        button_menu_1,
+        button_menu_3,
+        start_pop = 40,
+
+        //game 
+        start_game = 60, //fischietto
+        end_game
 
     }
 
