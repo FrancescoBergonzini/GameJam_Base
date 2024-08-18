3fbf0929747e6b92c9597605f295f592908907fb
FrancescoBergonzini <fbergonzinigames@gmail.com>
2024-08-18 16:10:58 +0200

    add button wip ui

---
+++ GameJam_2023/Assets/GMTK/ScalEnigma/Prefabs/Objects/Button.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace ScalEnigma
+{
+    public class Button : MonoBehaviour
+    {
+        GameObject target;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.layer == Layers.Player)
+            {
+                target.SetActive(true);
+            }
+        }
+    }
+}
+
